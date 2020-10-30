445ee3cc5a7e67863466ffd67a0f97b845af462a
Rory McCaffrey <rjmccaffrey@ucdavis.edu>
2020-10-30 10:19:43 -0400

    header

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 (function(){
-    "use strict";
+  document.getElementById('me').setAttribute('draggable', false);
 
+
+  const header = document.querySelector("header");
+  window.addEventListener("wheel", ()=>{
+    let currentScroll = window.pageYOffset;
+    let pos = 0;
+
+    if (currentScroll > pos){
+      header.style.top= "-50px";
+      header.style.opacity="0";
+      return;
+    }
+    else{
+      header.style.top = "0px";
+      header.style.opacity="1";
+      return;
+    }
+  });
 }());
