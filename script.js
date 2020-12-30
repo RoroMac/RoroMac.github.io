7dfea986bfdfef9112267774c3cc24bd5b3ea9ce
Rory McCaffrey <rjmccaffrey@ucdavis.edu>
2020-12-29 18:17:09 -0800

    fixed header

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
 (function(){
-  const header = document.querySelector("header");
-  window.addEventListener("wheel", ()=>{
-    let currentScroll = window.pageYOffset;
-    let pos = 0;
 
-    if (currentScroll > pos){
-      header.style.top= "-50px";
-      header.style.opacity="0";
-      return;
+  //header function
+  let pos = 0;
+  let dim = 1;
+  let header = document.querySelector("header");
+  window.addEventListener("wheel", ()=>{
+
+    let currentScroll = window.pageYOffset;
+
+    console.log('pos:'+ pos);
+    console.log('currentScroll:'+currentScroll);
+
+    if (dim == 1) {
+      header.style.top = "0px";
+      header.style.opacity="1"
     }
     else{
-      header.style.top = "0px";
-      header.style.opacity="1";
-      return;
+      header.style.top= "-50px";
+      header.style.opacity="0";
     }
+
+
+    if (pos < currentScroll) {
+      dim = 0
+    }
+    else if (pos > currentScroll) {
+      dim = 1;
+    }
+
+    console.log("dim:"+dim)
+    pos = currentScroll;
   });
 
-  let clicked = document.querySelector("clickable");
-  window.addEventListener("click", function(){
-    console.log('error');
-    clicked.classList.add = "overlay";
-  });
+  //fullscreen function
+
 }());
